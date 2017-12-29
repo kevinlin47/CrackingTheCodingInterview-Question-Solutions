9ceca92317351b7bad696db6e419a462b4fb7a95
Kevin Lin <kevinlin47@hotmail.com>
2017-12-29 15:30:14 -0500

    Update CTCI19.java

--- Chapter 1/CTCI19.java
+++ Chapter 1/CTCI19.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+My solution to cracking the coding interview question 1.9
  */
 package ctci.pkg19;
 
